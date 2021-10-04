ae73688605799046ff561b5ac6db3dc62cca40f5
Semyon Stanovov <sestanovov@gmail.com>
2021-10-04 16:08:50 +0300

    5. Date [#331992] - Lecture

---
+++ src/main/java/ru/job4j/date/HbmRun.java
@@ -0,0 +1,35 @@
+package ru.job4j.date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ru.job4j.date.model.Product;
+
+import java.util.List;
+import java.util.TimeZone;
+
+public class HbmRun {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+
+            List<Product> products = session.createQuery("from Product").list();
+            for (Product pr : products) {
+                System.out.println(pr);
+            }
+
+            session.getTransaction().commit();
+            session.close();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
